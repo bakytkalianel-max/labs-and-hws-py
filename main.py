b9b98f9169dd301a2b7844fff0843e503f5043fe
Anel <you@example.com>
2026-02-05 08:32:54 +0500

    Add solution for task 11

--- main.py
+++ main.py
@@ -7,3 +7,17 @@ words = lambda s: sum(
 )
 text="hello worl5s star1 fre2 pyth0n"
 print(words(text))
+
+#11
+def common_unique_chars(s1,s2):
+    result = ""
+    for ch in s1:
+        if (
+            ch in s2
+            and ch not in result
+            and not ch.isdigit()
+            and ch != " "
+        ):
+            result += ch
+    return result
+print(common_unique_chars("hello worl5s", "abs c1a5d"))
