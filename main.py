a816217dc17f92de157d4fa1c76ffdf7f36e6c32
Anel <you@example.com>
2026-02-05 08:18:11 +0500

    Add solution for task 10

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("hello world")
+#10
+words = lambda s: sum(
+    1 for x in s.split()
+    if any(ch.isdigit() for ch in x)
+    and not x[0].isdigit()
+    and len(x) >= 5
+)
+text="hello worl5s star1 fre2 pyth0n"
+print(words(text))
